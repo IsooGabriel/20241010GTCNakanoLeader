6e812a8ddb478b8fd46f943dcb0f55e41a67b987
GabuGabuNoMi <139324502+IsooGabriel@users.noreply.github.com>
2024-10-11 10:55:31 +0900

    PlayerClass＿Gabu追加
    メンバ変数だけ追加

---
+++ Assets/GabuSpass/PlayerClass_Gabu.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerClass_Gabu : MonoBehaviour
+{
+    private List<int> _a_currentCards;
+    private int _i_points;
+    private TurnManager turnManagare;
+    private CardManager cardManager;
+
+}
