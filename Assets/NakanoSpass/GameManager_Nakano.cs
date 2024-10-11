510b31c3a6fa73c00fa163e7f2b7146b6112e940
nkc-nakano <nakano831831@gmail.com>
2024-10-11 14:52:58 +0900

    GameManager更新

--- Assets/NakanoSpass/GameManager_Nakano.cs
+++ Assets/NakanoSpass/GameManager_Nakano.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class GameManager_Nakano : MonoBehaviour
 {
+    int PlayerPoint;//プレイヤーの合計ポイント(仮)
+    int DealerPoint;//ディーラーの合計ポイント(仮)
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +17,22 @@ public class GameManager_Nakano : MonoBehaviour
     void Update()
     {
         
+    }
+    void Judge()//勝敗
+    {
+        PlayerManagerScript playermanagerscript = GetComponent<PlayerManagerScript>();//PlayerManagerスクリプトを探す
+        DealerManagerScript dealermanagerscript = GetComponent<DealerManagerScript>();//DealerManagerスクリプトを探す
+        if (PlayerPoint< DealerPoint)//Playerの勝敗を調べる
+        {
+            Debug.Log("敗北");
+        }
+        else　if (PlayerPoint > DealerPoint)//Playerの勝敗を調べる
+        {
+            Debug.Log("勝利");
+        }
+        else
+        {
+            Debug.Log("同点");
+        }
     }
 }
