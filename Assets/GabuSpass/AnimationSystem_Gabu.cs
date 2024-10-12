392a7077ff8353efd54a9070feb6891ecbcd7bac
SA3 <sa3@gabugabunomi.com>
2024-10-12 14:04:19 +0900

    DOTween, UI, Scene追加

---
+++ Assets/GabuSpass/AnimationSystem_Gabu.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AnimationSystem_Gabu : MonoBehaviour
+{
+    public Animator animator;
+    public Button button;
+    public Image outLine;
+    public Image backGround;
+    public TextMeshProUGUI tmp;
+
+
+}
