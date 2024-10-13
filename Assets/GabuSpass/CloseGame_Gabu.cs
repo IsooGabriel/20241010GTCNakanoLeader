d420a0c65bcdba43d3af6d407c787903f20c1e8d
SA3 <sa3@gabugabunomi.com>
2024-10-13 16:34:47 +0900

    GameScene更新
    ゲームシーンからもゲームを終了できるようにした

---
+++ Assets/GabuSpass/CloseGame_Gabu.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloseGame_Gabu : MonoBehaviour
+{
+    public GameObject obj;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            obj.SetActive(obj.activeSelf);
+        }
+    }
+}
